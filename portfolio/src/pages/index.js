d8ac0002ec2ff279f2bdc5e8aed65ca49d6ad6cd
Rich Ekelman <37753567+servstudio@users.noreply.github.com>
2023-03-22 19:52:48 -0400

    centered main project grid

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -27,24 +27,26 @@ export default function Home({ projects }) {
         <Header />
         <div
           className={
-            "container mx-auto grid grid-cols-1 md:grid-cols-2 md:grid-rows-auto gap-8 p-12 md:p-32"
+            "container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:grid-rows-auto gap-8 p-8 md:p-8"
           }
         >
           {projects.map((project) => (
             // Project Card
             <>
               <Link key={project.id} href={`/${project.id}`}>
-                <div className="flex flex-col p-4 duration-300 ease-in-out hover:shadow-md">
+                <div className="flex flex-col p-4 duration-300 ease-in-out rounded-md stretch hover:shadow-md">
                   <img
                     className="bg-center bg-cover rounded-sm"
                     src={project.key_img}
                     alt={`${project.title} image`}
                   ></img>
                   <div className="pt-2">
-                    <h2 className="text-2xl text-black">{project.title}</h2>
-                    <p className="text-sm text-black line-clamp-2">
+                    <h2 className="text-xl font-medium text-black capitalize">
+                      {project.title}
+                    </h2>
+                    {/* <p className="text-sm text-black line-clamp-2">
                       {project.blurb}
-                    </p>
+                    </p> */}
                   </div>
                 </div>
               </Link>
