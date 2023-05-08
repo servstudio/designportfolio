9366c5bc52adb63187beb51817a6fca302ef0294
Rich Ekelman <37753567+servstudio@users.noreply.github.com>
2023-05-08 13:45:22 -0400

    Updated anlytics info.

--- portfolio/src/pages/_app.js
+++ portfolio/src/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Page>
         <Component {...pageProps} />
-        <Analytics id="Z2Y6XZ5ZQZ" />
+        <Analytics />
       </Page>
     </>
   )
