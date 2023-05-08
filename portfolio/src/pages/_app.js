a1cfa429a059ecba5d8481e0d2d23c407cd18362
Rich Ekelman <37753567+servstudio@users.noreply.github.com>
2023-05-08 13:43:13 -0400

    Initialzed vercel ananlytics on "_app.js" file.

--- portfolio/src/pages/_app.js
+++ portfolio/src/pages/_app.js
@@ -1,12 +1,14 @@
 import Page from "@/components/Page"
 import "@/styles/globals.css"
 import "tailwindcss/tailwind.css"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Page>
         <Component {...pageProps} />
+        <Analytics id="Z2Y6XZ5ZQZ" />
       </Page>
     </>
   )
