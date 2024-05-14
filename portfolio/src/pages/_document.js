f2f5107a6ff09d50c7c25f00ffc6c530ee921df4
Rich Ekelman <37753567+servstudio@users.noreply.github.com>
2024-05-13 21:58:05 -0400

    Pushing to build

--- portfolio/src/pages/_document.js
+++ portfolio/src/pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="../../public/flavicon.png" />
+        <link rel="icon" href="/public/flavicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,200;0,400;0,600;0,800;1,400&display=swap"
           rel="stylesheet"
